c5b6fee7fcac9cce7bb5aef5d26c3fea84e014c5
Ibad <ibadurrehman718@gmail.com>
2024-05-17 00:47:39 +0500

    layout added

---
+++ src/config/routes/routes.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from '../../screens/home/home'
+import Profile from '../../screens/profile/profile'
+import Login from '../../screens/login/login'
+import Forget from '../../screens/forget/forget'
+import Navbar from '../../components/Navbar'
+
+const Routers = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path='/dashboard' element={< Dashboard />} />
+                    <Route path='profile' element={<Profile />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/ForgetPassword' element={<Forget />} />
+                </Routes>
+
+            </BrowserRouter>
+        </>
+    )
+}
+export default Routers
